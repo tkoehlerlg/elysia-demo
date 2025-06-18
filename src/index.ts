0b0b823b4d991e40ed05f708bf434f32aade654a
Torben Köhler <torben@sonic.tech>
2025-06-18 08:43:17 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Elysia, t } from "elysia"
 import { tzDate } from "./tzDate"
 
-const app = new Elysia({ aot: false, encodeSchema: false }).get(
+const app = new Elysia({ aot: false, encodeSchema: true }).get(
     "/",
     () => ({ date: new Date() }),
     {
